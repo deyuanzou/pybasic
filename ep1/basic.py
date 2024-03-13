57a9f1bdb58892f89fb38520421c83a3b981d825
deyuanzou <3115729294@qq.com>
2024-03-13 11:23:07 +0800

    增添了Lexer类，定义了__init__、read_a_char、make_tokens方法

--- ep1/basic.py
+++ ep1/basic.py
@@ -9,6 +9,7 @@ TT_MUL = 'MUL'
 TT_DIV = 'DIV'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
+TT_UNDEFINED = 'UNDEFINED'
 
 
 class Token:
@@ -22,11 +23,42 @@ class Token:
         return f'{self.type}'
 
 
+#########################
+#  Lexer 词法分析器
+#########################
+class Lexer:
+    def __init__(self, text):
+        self.text = text  # text为输入文本
+        self.pos = -1  # pos为当前字符的位置
+        self.current_char = None  # current_char为当前字符
 
+    def read_a_char(self):
+        self.pos += 1
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
 
+    def make_tokens(self):
+        tokens = []
 
-
-
-
-
-
+        while self.current_char is not None:
+            if self.current_char in ' \t':
+                self.read_a_char()
+            elif self.current_char == '+':
+                tokens.append(Token(TT_PLUS, '+'))
+                self.read_a_char()
+            elif self.current_char == '-':
+                tokens.append(Token(TT_MINUS, '-'))
+                self.read_a_char()
+            elif self.current_char == '*':
+                tokens.append(Token(TT_MUL, '*'))
+                self.read_a_char()
+            elif self.current_char == '/':
+                tokens.append(Token(TT_DIV, '/'))
+                self.read_a_char()
+            elif self.current_char == '(':
+                tokens.append(Token(TT_LPAREN, '('))
+                self.read_a_char()
+            elif self.current_char == ')':
+                tokens.append(Token(TT_RPAREN, ')'))
+                self.read_a_char()
+            else:
+                tokens.append(Token(TT_UNDEFINED, 'undefined'))
