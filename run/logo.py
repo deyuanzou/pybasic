1884135f65aabb2547903bf77260888c71bd8ac0
deyuanzou <3115729294@qq.com>
2024-03-15 14:23:40 +0800

    增加了logo.py文件，定义了BASIC语言的logo

---
+++ run/logo.py
@@ -0,0 +1,8 @@
+def welcome():
+    print("""  
+        _____  _____  _____  _____  _____  
+       |     ||     ||     ||     ||     |  
+       |  B  ||  A  ||  S  ||  I  ||  C  |  
+       |_____||_____||_____||_____||_____|  
+    """)
+    print("Welcome to the BASIC Interpreter!")
